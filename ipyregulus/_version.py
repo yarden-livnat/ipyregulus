d8f21a6456d8084a649e4d84908e1ae4e7ac37ee
Yarden Livnat <yarden.livnat@gmail.com>
2018-08-13 17:33:02 -0600

    update version to 0.3.0

--- ipyregulus/_version.py
+++ ipyregulus/_version.py
@@ -1,7 +1,7 @@
 # Copyright (c)
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 2, 0, 'alpha', 0)
+version_info = (0, 3, 0, 'alpha', 0)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
