6d3032d558820b8356036482fdaa69e439b5e588
Yarden Livnat <yarden.livnat@gmail.com>
2018-08-13 18:51:34 -0600

    misc

--- ipyregulus/treetrait.py
+++ ipyregulus/treetrait.py
@@ -5,10 +5,12 @@ from .tree import TreeWidget
 class TreeTrait(TraitType):
     """A trait for a Regulus TreeWidget or a Tree"""
 
-    default_value = None
+    default_value = TreeWidget()
     info_text = 'a Tree or a TreeWidget'
 
     def validate(self, obj, value):
+        if value is None:
+            return TreeWidget()
         if isinstance(value, TreeWidget):
             return value
         if isinstance(value, Tree):
