1c74b1e2308c56f6d0ed3cbbe9f483625b8a28d6
Yarden Livnat <yarden.livnat@gmail.com>
2019-08-08 14:26:55 -0600

    fix build:lib command ensure html and css files are copied to the lib/

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,48 +1,11 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const dist_path = path.resolve(__dirname, 'lib');
 
 
-var rules = [
-  { test: /\.ts$/, loader: 'ts-loader', exclude: /node_modules/ },
-  { test: /\.js$/, loader: "source-map-loader" },
-  { test: /\.css$/,  use: [ 'style-loader', 'css-loader' ] },
-  { test: /\.html$/, use: {loader: 'raw-loader' }},
-  { test: /\.(png|svg|jpg|gif)$/, use: [ 'file-loader' ] }
-
-];
-
-const plugins = [];
-
-const resolve = {
-  extensions: ['.ts', '.js', '.css', '.scss', '.html']
-  // modules:['src', 'node_modules']
-};
-
-externals = [
-  '@jupyter-widgets/base',
-  'jupyter-scales', 'jupyter-datawidgets', 'jupyter-dataserializers'
-];
-
-nb= {
-  entry: './src/index.ts',
-  output: {
-    filename: 'index.js',
-    path: __dirname + '/../../ipyregulus/nbextension/static',
-    libraryTarget: 'amd'
-  },
-  module: {
-    rules: rules
-  },
-  devtool: 'source-map',
-  externals: ['@jupyter-widgets/base', 'jupyter-scales', 'jupyter-datawidgets', 'jupyter-dataserializers'],
-  resolve: {
-    extensions: [".webpack.js", ".web.js", ".ts", ".js", '.css', '.html']
-  }
-};
-
-const lab = {
+module.exports =  {
   entry:'./src/plugin.ts',
   output: {
     filename: 'labext.js',
@@ -55,16 +18,33 @@ const lab = {
   //    fs: 'empty',
   //  },
   module: {
-    rules
+    rules: [
+      { test: /\.ts$/, loader: 'ts-loader', exclude: /node_modules/ },
+      { test: /\.js$/, loader: "source-map-loader" },
+      { test: /\.css$/,  use: [ 'style-loader', 'css-loader' ] },
+      { test: /\.html$/, use: {loader: 'raw-loader' }},
+      { test: /\.(png|svg|jpg|gif)$/, use: [ 'file-loader' ] }
+
+    ]
   },
-  plugins,
-  resolve,
-  externals
+
+  plugins: [
+    new CopyPlugin([
+      {from: 'src/**/*.{html,css}', transformPath(target, abs) {
+          return target.substring(4);
+          }
+      }
+      ])
+  ],
+  resolve: {
+    extensions: ['.ts', '.js', '.css', '.scss', '.html']
+  },
+
+  externals: [
+   '@jupyter-widgets/base',
+    'jupyter-scales', 'jupyter-datawidgets', 'jupyter-dataserializers'
+  ]
   // externals: [nodeExternals(
   //   whitelist=['/^d3/']
   // )]
 };
-
-module.exports = [
-  lab
-];
