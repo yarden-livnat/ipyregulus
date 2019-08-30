977a9cfbb0178eb22a71a68f10277c37baff5ce4
Yarden Livnat <yarden.livnat@gmail.com>
2019-08-29 18:08:09 -0600

    improve filters move had_tree to the regulus package

--- ipyregulus/filters/basic_filters.py
+++ ipyregulus/filters/basic_filters.py
@@ -18,8 +18,9 @@ def Not(f):
     return lambda item: not f(item)
 
 
-class And(object):
-    def __init__(self, *args):
+class GroupFilter(object):
+    def __init__(self, name='Always', *args):
+        self.__name__= name
         self.filters = []
         self.add(*args)
 
@@ -34,26 +35,23 @@ class And(object):
         self.filters = []
 
     def __call__(self, *args, **kwargs):
+        return True
+
+
+class And(GroupFilter):
+    def __init__(self, *args):
+        super().__init__(name="All of", *args)
+
+    def __call__(self, *args, **kwargs):
         for f in self.filters:
             if not f(*args, **kwargs):
                 return False
         return True
 
 
-class Or(object):
+class Or(GroupFilter):
     def __init__(self, *args):
-        self.filters = []
-        self.add(*args)
-
-    def add(self, *args):
-        self.filters.extend(map(functor, args))
-
-    def remove(self, *args):
-        for f in args:
-            self.filters.remove(f)
-
-    def reset(self):
-        self.filters = []
+        super().__init__(name="Any of", *args)
 
     def __call__(self, *args, **kwargs):
         for f in self.filters:
